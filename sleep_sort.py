071734ac3154d855d14185ad0758467953ebf1ea
Viktor Roytman <vr2262@columbia.edu>
2016-09-11 17:24:43 -0400

    Fix 0 returning immediately.
    Add some documentation too.

--- sleep_sort.py
+++ sleep_sort.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
-"""Async version of the infamous sleep sort."""
+"""Async version of the infamous sleep sort.
+
+Turns out this is a roundabout heap sort:
+https://stackoverflow.com/questions/39398312
+"""
 import argparse
 import asyncio
 
 
+class FloatSpecialZero(float):
+    """Like float, but never compares equal to 0.
+
+    This is because asyncio.sleep treats 0 as a special case and returns
+    immediately.
+
+    https://github.com/python/cpython/blob
+    /e6d0995cc9f3416e7b10b3965e8cdf35a7eebf90/Lib/asyncio/tasks.py#L506
+    """
+
+    def __eq__(self, other):
+        return super().__eq__(other) if other else False
+
+
 async def sleepy(value):
     """Wait for as long as the value, then return it."""
-    return await asyncio.sleep(float(value), result=value)
+    return await asyncio.sleep(FloatSpecialZero(value), result=value)
 
 
 async def main(input_values, live=False):
